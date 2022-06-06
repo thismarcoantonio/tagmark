1f1173f183ebb40d1d1d85bdee8e15d6ced85893
thismarcoantonio <thismarcoantonio@gmail.com>
2022-06-06 20:32:29 -0300

    Add outlined alternative to TextBadge component

--- src/components/TextBadge.vue
+++ src/components/TextBadge.vue
@@ -1,14 +1,30 @@
 <template>
   <li
-    class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+    @click="handleClick"
+    :class="{
+      'bg-gray-200': !outlined,
+      'border border-gray-400': outlined,
+      'cursor-pointer': isButton,
+    }"
+    class="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
   >
-    <slot></slot>
+    <slot />
   </li>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from "vue-property-decorator";
+import { Vue, Component, Prop } from "vue-property-decorator";
 
 @Component
-export default class TextBadge extends Vue {}
+export default class TextBadge extends Vue {
+  @Prop({ default: false }) outlined!: boolean;
+
+  get isButton() {
+    return !!this.$listeners.click;
+  }
+
+  handleClick() {
+    this.$emit("click");
+  }
+}
 </script>
