e3d23cbc886bf95e9818766d35ac86c78132e755
thismarcoantonio <thismarcoantonio@gmail.com>
2022-06-06 21:04:14 -0300

    Add filter functionality to Searchbox

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,26 +1,55 @@
 <template>
-  <form class="relative flex items-center">
+  <form @submit.prevent="handleSubmit" class="relative flex items-center">
     <label
       :class="{ 'opacity-0 translate-x-1': value }"
-      class="absolute pointer-events-none transition duration-200 text-gray-400 left-2 select-none"
+      class="absolute pointer-events-none transition duration-200 text-gray-400 left-4 select-none"
     >
       Search by title or a tag
     </label>
     <input
+      :value="value"
+      @input="handleChange"
+      ref="search"
       type="search"
       name="search"
-      v-model="value"
       autocomplete="off"
-      class="p-2 w-full border border-slate-500 rounded-full outline-none"
+      class="pl-4 p-2 w-full border border-slate-500 text-gray-800 rounded-full outline-none"
     />
+    <button
+      type="submit"
+      class="absolute right-4 text-slate-500 hover:text-slate-800 transition-colors"
+    >
+      <remix-icon icon="search" />
+    </button>
   </form>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
+import { State, Action } from "vuex-class";
+import RemixIcon from "@/components/RemixIcon.vue";
 
-@Component
+@Component({
+  components: {
+    RemixIcon,
+  },
+})
 export default class SearchBox extends Vue {
   value = "";
+
+  @State("search", { namespace: "filter" })
+  search!: string;
+
+  @Action("setSearch", { namespace: "filter" })
+  setSearch!: (payload: string) => Promise<void>;
+
+  handleChange($event: Event) {
+    const inputValue = ($event.target as HTMLInputElement).value;
+    this.value = inputValue;
+  }
+
+  handleSubmit() {
+    this.setSearch(this.value);
+  }
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="home">
-    <search-box />
+    <search-box :readonly="true" />
     <div v-if="bookmarks.length" class="grid gap-4 my-8">
-      <router-link
-        :key="bookmark.id"
-        :to="`/bookmark/${bookmark.id}`"
-        v-for="bookmark in bookmarks"
-      >
-        <bookmark-item
-          :bookmark="bookmark"
-          :favorite="bookmark.favorite"
-          :tags="getBookmarkTags(bookmark)"
-          @toggle-favorite="handleFavorite(bookmark)"
-        />
-      </router-link>
+      <template v-if="bookmarkList.length">
+        <router-link
+          :key="bookmark.id"
+          :to="`/bookmark/${bookmark.id}`"
+          v-for="bookmark in bookmarkList"
+        >
+          <bookmark-item
+            :bookmark="bookmark"
+            :favorite="bookmark.favorite"
+            :tags="getBookmarkTags(bookmark)"
+            @toggle-favorite="handleFavorite(bookmark)"
+          />
+        </router-link>
+      </template>
+      <span class="text-slate-700" v-if="!bookmarkList.length">
+        Your active filters didn't match any bookmark. Update or clear your
+        filters.
+      </span>
     </div>
     <div class="mt-4 text-slate-700" v-if="!bookmarks.length">
       Looks like you didn't create any Bookmark yet,
@@ -26,7 +32,7 @@
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
-import { Action, State } from "vuex-class";
+import { Action, State, Getter } from "vuex-class";
 import { ActionMethod } from "vuex";
 import { Tag } from "@/declarations/Tag";
 import { Bookmark } from "@/declarations/Bookmark";
@@ -40,11 +46,14 @@ import BookmarkItem from "@/components/BookmarkItem.vue";
   },
 })
 export default class HomeView extends Vue {
+  @State("tags", { namespace: "tag" })
+  tags!: Tag[];
+
   @State("bookmarks", { namespace: "bookmark" })
   bookmarks!: Bookmark[];
 
-  @State("tags", { namespace: "tag" })
-  tags!: Tag[];
+  @Getter("bookmarkList", { namespace: "bookmark" })
+  bookmarkList!: Bookmark[];
 
   @Action("setFavorite", { namespace: "bookmark" })
   setFavorite!: ActionMethod;
